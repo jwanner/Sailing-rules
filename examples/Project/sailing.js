bf304a8b4b2c2b5eaeb96887c8d9a938a4f22eea
jwanner <Jacob@Jacobs-MacBook-Pro.local>
2014-12-27 22:54:33 -0500

    init commit

---
+++ examples/Project/sailing.js
@@ -0,0 +1,10 @@
+var point1 = [100, 150];
+var point2 = [115, 300];
+var point3 = [135, 400];
+var point4 = [450, 385];
+
+
+var path = new Path();
+path.add(new Point(point1),(new Point(point2), new Point(point3), new Point(point4));
+path.strokeColor = '#FFF';
+path.smooth();
